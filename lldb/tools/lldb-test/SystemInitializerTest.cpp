6a8a0aec8cbf7673edf67ff6ca876a09536ea1dd
Gabor Greif <ggreif@gmail.com>
2020-05-14 01:06:32 +0000

    fix gcc-9.3 compile errors

--- lldb/tools/lldb-test/SystemInitializerTest.cpp
+++ lldb/tools/lldb-test/SystemInitializerTest.cpp
@@ -259,7 +259,7 @@ llvm::Error SystemInitializerTest::Initialize() {
 
 void SystemInitializerTest::Terminate() {
   static Timer::Category func_cat(LLVM_PRETTY_FUNCTION);
-  Timer scoped_timer(func_cat, LLVM_PRETTY_FUNCTION);
+  Timer scoped_timer(func_cat, "%s", LLVM_PRETTY_FUNCTION);
 
   Debugger::SettingsTerminate();
 
